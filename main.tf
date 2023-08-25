710fdb4dffdb7a6b944290e5a4122ef51fb9e930
Shiv Gupta <95048591+ShivGupta76@users.noreply.github.com>
2023-08-25 16:52:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-  # GCS backend configuration has the following key-value pairs.
-
-  # Bucket: Google storage bucket name. previously created GCS bucket name
-  # Prefix: Folders inside the bucket.  TF will create this folder path inside provided bucket name above
-  # Credentials: Path to google service account file. It's mandatory because it has to access existing GCS on GCP 
-
-  # the reason credentials are needed in both the backend configuration and the provider block is because they serve different purposes 
-  # and are used in different parts of the Terraform process:
-
-  # Backend Configuration Credentials: Used to authenticate Terraform when interacting with the backend storage where your state files are stored.
-  # Provider Block Credentials: Used to authenticate Terraform with the cloud provider's APIs to manage resources within your cloud environment.
-  # Each set of credentials ensures that Terraform can perform its specific tasks securely and with the necessary permissions. 
-terraform {
-  backend "gcs" {
-    bucket = "tf-states-demo"  # create gs://tf-states-demo
-    prefix = "terraform/state" # create folders "terraform/state" --> gs://tf-states-demo/terraform/state/
-    ######## On run "Terraform init", TF will put default state at gs://tf-states-demo/terraform/state/default.tfstate 
-    credentials = "zinc-arc-396916-9d8a70ea2239.json"
-  }
-}
-
 provider "google" {
   project     = "zinc-arc-396916"
   credentials = file(var.credentials_file)
@@ -52,7 +31,7 @@ variable "region" {
   default     = "us-west4"
 }
 locals {
-  instance_name = "my-first-vm2"
+  instance_name = "my-first-vm1"
   instance_zone = "us-central1-a"
   machine_type  = "e2-medium"
   image         = "ubuntu-os-cloud/ubuntu-2004-lts"
@@ -61,3 +40,9 @@ locals {
     app = "web"
   }
 }
+
+
+
+
+
+
